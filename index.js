d8e4ecf08431396eaada41c842469a6abef1b632
Ayana Bando <ayanaflax@yahoo.com>
2019-01-18 17:44:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 function loadIssues() {
-  
- 
-  var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);  for(var i=0;i<issues.length;i++) {
-    var result = template(issues[i]);
+  var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
+  var result = template(issues);
   document.getElementsByTagName("main")[0].innerHTML += result;
+}
 }
 }
